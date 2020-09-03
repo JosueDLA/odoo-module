7239addd8dd75487b1974c23c2fe7dd70913051b
JosueDLA <josuedlavs@gmail.com>
2020-09-03 11:38:13 -0600

    Created Services list

--- respartner.py
+++ respartner.py
@@ -3,9 +3,19 @@ from odoo import fields, models, api
 
 
 class Partner(models.Model):
+    _name = 'res.partner'
     _inherit = 'res.partner'
+
+    servicios = fields.One2many(
+        'res.partner.services', 'service_id', string='Servicios')
+
+
+class Partner_list():
+    _name = 'res.partner.services'
 
     service = fields.Char(required=False, string="Servicio")
     url = fields.Char(required=False, string="URL")
     user = fields.Char(required=True, string="Usuario")
     password = fields.Char(required=True, string="Contraseña")
+
+    service_id = fields.Many2one('res.partner', string='Partner Id')
